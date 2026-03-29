a39f5b2cb1933f5ff70b3162bb9a179819d9ba32
naomi <orest.chupa@gmail.com>
2026-03-29 15:05:20 +0300

    Resolve naming

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors": [format_linter_error(x) for x in errors],
+        "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
         "status": "failed" if len(errors) > 0 else "passed",
     }
